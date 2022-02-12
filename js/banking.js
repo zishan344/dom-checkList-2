b150a083af6fd5318b707edc7deefad89056ec6a
zishan344 <zishanahmed344@gmail.com>
2022-02-12 10:44:18 +0600

    done deposite section

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,25 @@
+function input(inputId) {
+  const idInput = document.getElementById(inputId);
+  const inputIdTExt = parseFloat(idInput.value);
+  idInput.value = "";
+  return inputIdTExt;
+}
+function updateTotal(total, depositAmount) {
+  const totalMoney = document.getElementById(total);
+  const totalMoneyText = parseFloat(totalMoney.innerText);
+  const totals = totalMoneyText + depositAmount;
+  totalMoney.innerText = totals;
+}
+function Balance(amount) {
+  const balanceTotal = document.getElementById("balance-total");
+  const balanceTotalText = parseFloat(balanceTotal.innerText);
+  const previousBalance = balanceTotalText + amount;
+  balanceTotal.innerText = previousBalance;
+}
 document
   .getElementById("deposite-button")
   .addEventListener("click", function () {
-    console.log("done");
+    const depositAmount = input("deposite-input");
+    updateTotal("deposite-total", depositAmount);
+    Balance(depositAmount);
   });
